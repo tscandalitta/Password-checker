ae62cec36fb07f17965160c57f0523feeee3f030
tscandalitta <tscandalitta@gmail.com>
2020-04-19 20:22:55 -0300

    agrego funcion checkPassword()

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@ function togglePassword() {
     var pwd = document.getElementById("password-input");
     var icon = document.getElementById("password-icon");
     if (pwd.type === "password") {
-      pwd.type = "text";
-      icon.className = "fa fa-eye-slash pwd-icon";
+        pwd.type = "text";
+        icon.className = "fa fa-eye-slash pwd-icon";
     } else {
-      pwd.type = "password";
-      icon.className = "fa fa-eye pwd-icon";
+        pwd.type = "password";
+        icon.className = "fa fa-eye pwd-icon";
     }
-  }
+}
+
+function checkPassword() {
+    var pwdInput = document.getElementById("password-input");
+    var password = pwdInput.value;
+    alert("Prueba, su password es: "+password);
+}
