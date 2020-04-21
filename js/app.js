3cc30c4838b55adfd6d4746bb3d19fc4d6d8106a
tscandalitta <tscandalitta@gmail.com>
2020-04-21 16:49:40 -0300

    agrego funcionalidad para mostrar las passwords almacenadas en el navegador

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 
+var passwordsAlmacenadas = 5;
+var keylocalStorage = "passwords";
+var tablaPasswords = document.getElementById("tablaPasswords");
+var tablaPasswordsBody = document.getElementById("tablaPasswordsBody");
+
+function actualizarTabla(){
+    var pwdArray = getPasswords();
+        if(pwdArray !== null) {
+            while(tablaPasswordsBody.hasChildNodes()) {
+                tablaPasswordsBody.removeChild(tablaPasswordsBody.firstChild);
+            }
+            for (var i in pwdArray) {
+                var tr = document.createElement('TR');
+                var td = document.createElement('TD');
+                td.appendChild(document.createTextNode(pwdArray[i]));
+                tr.appendChild(td);
+                tablaPasswordsBody.appendChild(tr);
+            }
+        }
+}
+
 function togglePassword() {
     var password = document.getElementById("password-input");
     var icon = document.getElementById("password-icon");
@@ -15,21 +36,32 @@ function checkPassword() {
     var pwdInput = document.getElementById("password-input");
     var password = pwdInput.value;
     if(password != "") {
-        alert("Prueba, su password es: " + password);
-        almacenarPassword(password);
+        checkStrenght(password);
+        storePassword(password);
+        actualizarTabla();
     }
 }
 
-var keylocalStorage = "passwords";
-function almacenarPassword(password) {  
+function checkStrenght(password){
+    alert("Prueba, su password es: " + password);
+}
+
+
+function storePassword(password) {  
         var pwdArray = getPasswords();
         if(pwdArray !== null) {
             pwdArray.push(password);
+            if(pwdArray.length > passwordsAlmacenadas)
+                pwdArray.shift();
             localStorage.setItem(keylocalStorage, JSON.stringify(pwdArray));
         } else
             alert("El localStorage no está habilitado en su navegador");   
 }
 
+/**
+ * Devuelve un arreglo con las passwords almacenas en el localStorage
+ * En caso de no poder acceder al almacenamiento, retorna null
+ */
 function getPasswords() {
     if (typeof(Storage) !== "undefined") {  
         var pwdArray = JSON.parse(localStorage.getItem(keylocalStorage));
