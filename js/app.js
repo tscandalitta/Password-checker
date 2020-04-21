acc08b09a7accc9adfbb19ceed775fa54bb7b4c2
tscandalitta <tscandalitta@gmail.com>
2020-04-21 19:35:06 -0300

    fix arreglo contraseñas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 
-var passwordsAlmacenadas = 5;
+var passwordsAlmacenadas = 4;
 var keylocalStorage = "passwords";
 var tablaPasswords = document.getElementById("tablaPasswords");
 var tablaPasswordsBody = document.getElementById("tablaPasswordsBody");
-actualizarTabla();
+init();
 
-function actualizarTabla(){
+function init(){
+    actualizarTablaPasswords();
+    inicializarHeaderPasswords();
+}
+
+function inicializarHeaderPasswords(){
+    var headerPasswords = document.getElementById("tablaPasswordsHeader");
+    headerPasswords.innerHTML = "Últimas " + passwordsAlmacenadas + " contraseñas";
+}
+
+function actualizarTablaPasswords(){
     var pwdArray = getPasswords();
         if(pwdArray !== null) {
             while(tablaPasswordsBody.hasChildNodes()) {
@@ -39,7 +49,7 @@ function checkPassword() {
     if(password != "") {
         checkStrenght(password);
         storePassword(password);
-        actualizarTabla();
+        actualizarTablaPasswords();
     }
 }
 
@@ -51,14 +61,24 @@ function checkStrenght(password){
 function storePassword(password) {  
         var pwdArray = getPasswords();
         if(pwdArray !== null) {
-            pwdArray.push(password);
-            if(pwdArray.length > passwordsAlmacenadas)
-                pwdArray.shift();
+            insertar(pwdArray,password);
             localStorage.setItem(keylocalStorage, JSON.stringify(pwdArray));
         } else
             alert("El localStorage no está habilitado en su navegador");   
 }
 
+/**
+ * Almacena la contraseña en el arreglo. 
+ * Si se achica el tamaño del arreglo, pueden quedar contraseñas almacenadas
+ * en el localStorage por lo que las elimina hasta cumplir con el tamaño deseado.
+ * @param {array} array - arreglo de contraseñas
+ * @param {string} password - contraseña a almacenar en el arreglo 
+ */
+function insertar(array, password) {
+    array.push(password);
+    while(array.length > passwordsAlmacenadas)
+        array.shift();
+}
 /**
  * Devuelve un arreglo con las passwords almacenas en el localStorage
  * En caso de no poder acceder al almacenamiento, retorna null
