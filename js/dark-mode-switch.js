4ba4747332f9136faa8c6cc2a181b0849126c382
tscandalitta <tscandalitta@gmail.com>
2020-05-08 15:31:08 -0300

    cambio boton de modo oscuro

--- js/dark-mode-switch.js
+++ js/dark-mode-switch.js
@@ -1,40 +1,49 @@
-(function() {
-    var darkSwitch = document.getElementById("darkSwitch");
-    if (darkSwitch) {
-        initTheme();
-        darkSwitch.addEventListener("change", function(event) {
-            resetTheme();
-        });
-        function initTheme() {
-            var darkThemeSelected =
-                localStorage.getItem("darkSwitch") !== null &&
-                localStorage.getItem("darkSwitch") === "dark";
-            darkSwitch.checked = darkThemeSelected;
-            darkThemeSelected
-                ? document.body.setAttribute("data-theme", "dark")
-                : document.body.removeAttribute("data-theme");
-            actualizarColores();
+(function () {
+    var iconoAmarillo = "icono-amarillo";
+    var iconoGrisOscuro = "icono-gris-oscuro";
+
+    $("#icono-tema").click(function () {
+        $(this).toggleClass("fa-moon fa-sun");
+        $("#span-tema").toggleClass(iconoAmarillo + " " + iconoGrisOscuro);
+        resetTheme();
+    });
+    initTheme();
+
+    function initTheme() {
+        var darkThemeSelected =
+            localStorage.getItem("darkSwitch") !== null &&
+            localStorage.getItem("darkSwitch") === "dark";
+
+        if (darkThemeSelected) {
+            document.body.setAttribute("data-theme", "dark");
+            $("#span-tema").addClass(iconoAmarillo);
+            $("#icono-tema").addClass("fa-sun");
+        } else {
+            document.body.removeAttribute("data-theme");
+            $("#span-tema").addClass(iconoGrisOscuro);
+            $("#icono-tema").addClass("fa-moon");
+        }            
+        actualizarColores();
+    }
+
+    function resetTheme() {
+        if ($("body").attr("data-theme") === "dark") {
+            document.body.removeAttribute("data-theme");
+            localStorage.removeItem("darkSwitch");
+        } else {
+            document.body.setAttribute("data-theme", "dark");
+            localStorage.setItem("darkSwitch", "dark");
         }
-        function resetTheme() {
-            if (darkSwitch.checked) {
-                document.body.setAttribute("data-theme", "dark");
-                localStorage.setItem("darkSwitch", "dark");
-            } else {
-                document.body.removeAttribute("data-theme");
-                localStorage.removeItem("darkSwitch");
-            }
-            actualizarColores();
+        actualizarColores();
+    }
+
+    function actualizarColores() {
+        if (document.body.getAttribute("data-theme") === "dark") {
+            $("table").addClass("table-dark");
+            $("thead").removeClass("thead-light");
+        } else {
+            $("table").removeClass("table-dark");
+            $("thead").addClass("thead-light");
         }
     }
 })();
-
-function actualizarColores() {
-    if(document.body.getAttribute("data-theme") === "dark") {
-        $("table").addClass("table-dark");
-        $("thead").removeClass("thead-light");
-    } 
-    else {
-        $("table").removeClass("table-dark");
-        $("thead").addClass("thead-light");
-    }
-}
